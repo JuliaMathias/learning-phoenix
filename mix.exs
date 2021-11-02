532a19c4f7c70c04337ca3817bb43ec63e8a5466
JuliaMathias <juliamss@gmail.com>
2021-11-02 20:18:01 -0300

    add dyalixir

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Today.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_deps: :transitive]
     ]
   end
 
@@ -46,7 +47,8 @@ defmodule Today.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
     ]
   end
 
