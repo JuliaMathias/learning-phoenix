1b3b9bf12c3ba4fab8bb7a9e342a6e20047823f0
JuliaMathias <juliamss@gmail.com>
2021-11-02 16:25:42 -0300

    create hello controller

---
+++ lib/today_web/controllers/hello_controller.ex
@@ -0,0 +1,8 @@
+defmodule TodayWeb.HelloController do
+  use TodayWeb, :controller
+
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
