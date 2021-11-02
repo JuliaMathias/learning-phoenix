433ddfbc99ec8d9648c89516b26942d78d7ac473
JuliaMathias <juliamss@gmail.com>
2021-11-02 19:54:37 -0300

    add locale plug to router

--- lib/today_web/router.ex
+++ lib/today_web/router.ex
@@ -8,6 +8,7 @@ defmodule TodayWeb.Router do
     plug :put_root_layout, {TodayWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug TodayWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
